03b3740148675c22d49f4e93ba414d148a55af77
di-birt <mardanza@birt.eus>
2026-09-17 10:22:48 +0200

    TE modificado por TA

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'DI04_TE02_2627',
+  appName: 'DI04_TA02_2627',
   webDir: 'www'
 };
 
